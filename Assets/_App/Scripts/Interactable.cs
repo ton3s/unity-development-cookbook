8fcd0e298282678348f502752d9cef31e11b65ba
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-11-30 21:49:22 -0600

    Trigger functionality

---
+++ Assets/_App/Scripts/Interactable.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Interactable : MonoBehaviour
+{
+	public void Interact(GameObject fromObject)
+	{
+		Debug.Log($"I've ({this.gameObject}) been interacted with {fromObject}");
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.LogFormat("Object {0} entered trigger {1}!",
+						other.name, this.name);
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		Debug.LogFormat("Object {0} exited trigger {1}!",
+						other.name, this.name);
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		Debug.LogFormat("Object {0} remained in trigger {1}!",
+					other.name, this.name);
+	}
+}
